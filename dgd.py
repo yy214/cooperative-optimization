9cd212c4e3d6e45e133152cfe39746335e38376b
Dimitri <d.desaintguilhem@gmail.com>
2025-04-01 16:44:37 +0200

    derniere modifs pour plot

--- dgd.py
+++ dgd.py
@@ -65,12 +65,12 @@ def compute_gap(xi, X,y,x_selected):
     gap = np.sqrt(np.sum((x_k - x_star) ** 2, axis=1))
     return gap
 
-def plot_gap(gaps):
+def plot_gap(gaps, labels):
 
     plt.figure(figsize=(8, 6))
 
     for k in range(len(gaps)):
-        plt.loglog(np.arange(len(gaps[k])), gaps[k], linestyle='-', label = str(k))
+        plt.loglog(np.arange(len(gaps[k])), gaps[k], linestyle='-', label = labels[k])
     plt.xlabel('number of steps')
     plt.ylabel(r'Gap $||\alpha_i - \alpha*||$')
     plt.title('optimality gap of DGD')
@@ -78,10 +78,8 @@ def plot_gap(gaps):
     plt.legend()
     plt.show()
 
-def diminishing_step_size(k):
-    return 0.002/(0.001*k+1)
 
-def dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count):
+def dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count, step_size, laplace_scale):
     xi = np.random.normal(0, 0.01, size=(step_count+1, a, m))
     g = np.zeros((a, m))
     # local gradients
@@ -90,7 +88,7 @@ def dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count):
 
     W_hat = W - np.identity(W.shape[0])
     for k in range(step_count):
-        val_step_size = dim_step_size(k)
+        val_step_size = step_size(k)
         var = laplace_scale(k)
         gamma = dim_gamma(k)
         laplacian_noise = np.random.laplace(loc = 0.0, scale = var, size = (a,m))
@@ -101,10 +99,16 @@ def dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count):
             g[i, :] = grad_fi(y, Kmm, Knm, xi[k+1,i, :], ids_agent)
     return xi
 
-def laplace_scale(iter):
+def laplace_scale1(iter):
     return 1+0.001*iter**0.3
+def laplace_scale2(iter):
+    return (1+0.001*iter**0.3)/np.sqrt(10)
+def laplace_scale3(iter):
+    return (1+0.001*iter**0.3)/10
 def dim_step_size(iter):
     return 0.002/(1+0.001*iter)
+def const_step_size(iter):
+    return 0.002
 def dim_gamma(iter):
     return 0.002/(1+0.001*iter**0.9)
 
@@ -118,8 +122,7 @@ if __name__=="__main__":
     n = 100
     m = 10
     a = 5 # nb of agents
-    step_size = 0.002 
-    step_count = 100000
+    step_count = 10000
 
     # selected indexes for calculations
     sel = list(range(n))
@@ -139,21 +142,57 @@ if __name__=="__main__":
 
     # dgd
 
-    xi_cst_step_size = dgd(a,m,agents,Kmm, Knm,W,y, step_count, step_size)
-    # xi_dim_step_size = dgd(a,m,agents,Kmm, Knm,W,y, step_count, diminishing_step_size, constant_step_size=False)
-    # affichage
+    gaps = []
+    labels = [r'$\epsilon = 10$', r'$\epsilon = 1$', r'$\epsilon = 0.1$']
 
-    # plot(agents, X,y, xi_dim_step_size, step_count)
-
-    # gaps = [compute_gap(xi_cst_step_size, X[sel],y[sel],x_selected), compute_gap(xi_dim_step_size, X[sel],y[sel],x_selected)]
-    # plot_gap(gaps)
-
-
-
-    xi_dgd_dp = dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count)
-    gaps = [compute_gap(xi_dgd_dp, X[sel],y[sel],x_selected), compute_gap(xi_cst_step_size, X[sel],y[sel],x_selected)]
-    plot_gap(gaps)
-
+    xi_cst_step_size = dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count, const_step_size, laplace_scale1)
+    # xi_dgd_dp = dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count)
+    gaps.append(compute_gap(xi_cst_step_size, X[sel],y[sel],x_selected))
+    xi_cst_step_size = dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count, const_step_size, laplace_scale2)
+    # xi_dgd_dp = dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count)
+    gaps.append(compute_gap(xi_cst_step_size, X[sel],y[sel],x_selected))
+    xi_cst_step_size = dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count, const_step_size, laplace_scale3)
+    # xi_dgd_dp = dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count)
+    gaps.append(compute_gap(xi_cst_step_size, X[sel],y[sel],x_selected))
 
 
     
+    somme = 0
+    for t in range(step_count):
+        somme += 2*dim_step_size(t)*laplace_scale1(t)**2
+    print("epsilon 1 :", somme)
+    somme = 0
+    for t in range(step_count):
+        somme += 2*dim_step_size(t)*laplace_scale2(t)**2
+    print("epsilon 1 :", somme)
+    somme = 0
+    for t in range(step_count):
+        somme += 2*dim_step_size(t)*laplace_scale3(t)**2
+    print("epsilon 1 :", somme)
+
+    plot_gap(gaps, labels)
+    gaps = []
+    labels = [r'$\epsilon = 10$', r'$\epsilon = 1$', r'$\epsilon = 0.1$']
+
+    xi_cst_step_size = dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count, dim_step_size, laplace_scale1)
+    # xi_dgd_dp = dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count)
+    gaps.append(compute_gap(xi_cst_step_size, X[sel],y[sel],x_selected))
+    xi_cst_step_size = dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count, dim_step_size, laplace_scale2)
+    # xi_dgd_dp = dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count)
+    gaps.append(compute_gap(xi_cst_step_size, X[sel],y[sel],x_selected))
+    xi_cst_step_size = dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count, dim_step_size, laplace_scale3)
+    # xi_dgd_dp = dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count)
+    gaps.append(compute_gap(xi_cst_step_size, X[sel],y[sel],x_selected))
+    
+    plot_gap(gaps, labels)
+
+
+    gaps = []
+    labels = ["DGD", r"DGD DP, $\epsilon = 1$"]
+    xi_cst_step_size = dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count, dim_step_size, laplace_scale2)
+    gaps.append(compute_gap(xi_cst_step_size, X[sel],y[sel],x_selected))
+    xi_dgd_dp = dgd_dp(a,m,agents,Kmm, Knm,W,y, step_count, dim_step_size, laplace_scale2)
+    gaps.append(compute_gap(xi_dgd_dp, X[sel],y[sel],x_selected))
+
+    plot_gap(gaps, labels)
+
